dd690d112d5729c44a6fb25a4c2f23ec01348f1a
PracticumTutor <PracticumTutor@gmail.com>
2025-08-01 08:20:19 -0400

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span className="footer__author">Developed by Myo Myat Myat Thant</span>
+      <span className="footer__year">2025</span>
+    </footer>
+  );
+}
+
+export default Footer;
