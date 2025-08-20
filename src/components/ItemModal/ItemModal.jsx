f9a85d54df95789bbec6a1d66ff4c664f4541689
PracticumTutor <PracticumTutor@gmail.com>
2025-08-20 09:45:36 -0400

    adjusted modals

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -18,8 +18,10 @@ function ItemModal({ activeModal, onClose, card, onDeleteItem }) {
         </button>
         <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+          <div className="modal__info">
+            <h2 className="modal__caption">{card.name}</h2>
+            <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
           <button
             onClick={handleDelete}
             className="modal__delete-btn"
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -11,7 +11,7 @@ function ModalWithForm({
 }) {
   return (
     <div className={`modal ${isOpen ? "modal__opened" : ""}`}>
-      <div className="modal__content">
+      <div className="modal__content modal__content_type_form">
         <h2 className="modal__title">{title}</h2>
         <button onClick={onClose} className="modal__close" type="button">
           <img src={crossIcon} alt="Close" className="modal__close-icon" />
